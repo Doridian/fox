b80aae7f1e8949b4b92775ceb56e369ac0887e8b
Doridian <git@doridian.net>
2025-02-13 16:24:15 -0800

    defaults should be easy

--- shell/base.go
+++ shell/base.go
@@ -76,7 +76,7 @@ func (s *Shell) init() {
 	s.print = s.l.GetGlobal("print").(*lua.LFunction)
 
 	mainMod := loader.NewLuaModule()
-	mainMod.AddModule(s.l, s, loader.DefaultLuaModuleConfig())
+	mainMod.AddModuleDefault(s.l, s)
 	mainMod.Load(s.l)
 	s.mainMod = mainMod
 
