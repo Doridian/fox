65398ff95d4b27831cd0171aed89a79bc14f1871
Doridian <git@doridian.net>
2025-02-13 19:26:06 -0800

    Stop creating globals

--- shell/init.lua
+++ shell/init.lua
@@ -20,8 +20,8 @@ package.cpath = ""
 
 shell.parsers = shell.parsers or {}
 function shell.parsers.lua(cmd)
-    cmdLen = cmd:len()
-    cmdLastTwo = cmd:sub(cmdLen - 1, cmdLen)
+    local cmdLen = cmd:len()
+    local cmdLastTwo = cmd:sub(cmdLen - 1, cmdLen)
     if cmdLastTwo == "\n\n" then
         return cmd
     end
@@ -30,8 +30,8 @@ end
 
 function shell.parser(cmd)
     if cmd:sub(1, 1) == "!" then
-        newLine = cmd:find("\n", 1, true)
-        cmdPrefix = cmd:sub(2, newLine - 1)
+        local newLine = cmd:find("\n", 1, true)
+        local cmdPrefix = cmd:sub(2, newLine - 1)
         if shell.parsers[cmdPrefix] then
             return shell.parsers[cmdPrefix](cmd:sub(newLine + 1))
         end
