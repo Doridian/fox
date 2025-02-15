6679e65ccd90c7d5378d3eefed12d4fbe5cb39cf
Doridian <git@doridian.net>
2025-02-15 09:22:35 -0800

    Switch it up to just change arg0 meaning

--- fox/main.go
+++ fox/main.go
@@ -3,17 +3,50 @@ package fox
 import (
 	"flag"
 	"os"
+	"strconv"
 
 	"github.com/Doridian/fox/modules/loader"
 	"github.com/Doridian/fox/shell"
 )
 
+const (
+	EvalUnset = iota
+	EvalAsFile
+	EvalAsString
+	EvalAsCommand
+)
+
+var evalType = EvalUnset
+
+func setEvalType(s string, newType int) error {
+	v, err := strconv.ParseBool(s)
+	if err != nil {
+		return err
+	}
+	if !v {
+		return nil
+	}
+	evalType = newType
+	return nil
+}
+
 var continuePtr = flag.Bool("k", false, "Keep running after script")
 var gomodsGlobal = flag.Bool("gomods-global", true, "Register go modules as globals")
 var gomodsAutoLoad = flag.Bool("gomods-auto-load", true, "Automatically load go modules")
-var commandPtr = flag.String("c", "", "Command to run")
 
 func Main() error {
+	flag.BoolFunc("c", "First arg is an internal command", func(s string) error {
+		return setEvalType(s, EvalAsCommand)
+	})
+	flag.BoolFunc("e", "First arg is code to evaluate", func(s string) error {
+		return setEvalType(s, EvalAsString)
+	})
+	flag.BoolFunc("f", "First arg is file to run", func(s string) error {
+		return setEvalType(s, EvalAsFile)
+	})
+
+	var err error
+
 	flag.Parse()
 
 	cfg := loader.DefaultConfig()
@@ -21,16 +54,24 @@ func Main() error {
 	cfg.AutoLoad = *gomodsAutoLoad
 	loader.SetDefaultConfig(cfg)
 
-	s := shell.New(flag.Args())
-
-	command := *commandPtr
-	if command != "" {
-		return s.RunCommand(command)
+	args := []string{}
+	if flag.NArg() > 1 {
+		args = flag.Args()[1:]
 	}
+	s := shell.New(args)
 
-	runScript := flag.Arg(0)
-	if runScript != "" {
-		err := s.RunFile(runScript)
+	if flag.NArg() > 0 {
+		arg0 := flag.Arg(0)
+		switch evalType {
+		case EvalAsString:
+			err = s.RunString(arg0)
+		case EvalAsCommand:
+			err = s.RunCommand(arg0)
+		default:
+			fallthrough
+		case EvalAsFile:
+			err = s.RunFile(arg0)
+		}
 		if !*continuePtr {
 			return err
 		}
