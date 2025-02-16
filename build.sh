db52f8fdbe953956f95031ad8d0e688cf963ff55
Doridian <git@doridian.net>
2025-02-16 01:26:20 -0800

    fix spam

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-set -ex
+set -e
 
+echo 'Sourcing PKGBUILD'
 source ./PKGBUILD
 srcdir=~/.local/bin
+startdir="$(pwd)"
+echo 'prepare()'
 prepare
+echo 'build()'
 build
+echo 'Done, exec...'
 
 exec fox
