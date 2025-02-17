01abc07ba17ace9e2b74cba9980b888d8cfe358a
Doridian <git@doridian.net>
2025-02-16 20:10:57 -0800

    Version is now also runDirect

--- modules/embed/root/commands/version.lua
+++ modules/embed/root/commands/version.lua
@@ -2,10 +2,8 @@ local info = require("go:info")
 
 local M = {}
 
-function M.run()
-    print("version: " .. info.version)
-    print("commit: " .. info.gitrev)
-    return 0
+function M.runDirect()
+    return 0, "version: " .. info.version .. "\ncommit: " .. info.gitrev .. "\n"
 end
 
 return M
