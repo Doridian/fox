8c5282c599dc105b916477e72e09ac4d09193598
Doridian <git@doridian.net>
2025-02-19 22:21:21 -0800

    a

--- modules/embed/root/commands/sh.lua
+++ modules/embed/root/commands/sh.lua
@@ -4,6 +4,7 @@ local goshell = require("go:shell")
 local M = {}
 
 function M.run(_, script)
+    -- TOOD: Change this to argOffset param in shell module
     goshell.popArgs(2)
     shell.runLine(script)
 end
